a3955aab15e3079a6edb2fe67ad3781888f6da8b
akimoto-tt <56018302+akimoto-tt@users.noreply.github.com>
2020-08-21 22:28:46 +0900

    ボタンで遷移先のアクティビティを分岐するようにしましたがエラーが出ます

--- app/src/main/java/com/example/pozcons_game/StageActivity.java
+++ app/src/main/java/com/example/pozcons_game/StageActivity.java
@@ -7,7 +7,28 @@ import android.widget.BaseAdapter;
 import android.widget.ListView;
 
 
-    public void startGame(View view) {
+//それぞれのidを設定**/
+        Button button1=(Button)findViewById(R.id.button1);
+                Button button2=(Button)findViewById(R.id.button2);
+                //ボタンが押されたらonClickが動作するよう設定**/
+                button1.setOnClickListener(this);
+                button2.setOnClickListener(this);
+                }
+
+//ボタンが押された時の処理**/
+public void onClick(View v){
+        switch(v.getId()){
+
+        case R.id.button1:
+public void startGame(View view) {
         startActivity(new Intent(getApplicationContext(), MainActivity.class));
-    }
+        }
+        break;
+
+        case R.id.button2:
+public void startGame(View view) {
+        startActivity(new Intent(getApplicationContext(), MainActivity2.class));
+        }
+        break;
+
 }
