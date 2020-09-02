a9272f8cf54c03b5a2db81498d437cb73bbbe3db
akimoto-tt <56018302+akimoto-tt@users.noreply.github.com>
2020-09-02 17:20:04 +0900

    レイアウトのエラーは消えましたがJavaの方にエラーが出ました

--- app/src/main/java/com/example/pozcons_game/StageActivity.java
+++ app/src/main/java/com/example/pozcons_game/StageActivity.java
@@ -3,10 +3,12 @@ package com.example.pozcons_game;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
+import android.view.View.OnClickListener;
 import android.widget.BaseAdapter;
 import android.widget.LinearLayout;
 import android.widget.Button;
-
 
 //それぞれのidを設定**/
         Button button1=(Button)findViewById(R.id.button1);
@@ -14,7 +16,14 @@ import android.widget.Button;
                 //ボタンが押されたらonClickが動作するよう設定**/
                 button1.setOnClickListener(this);
                 button2.setOnClickListener(this);
-                }
+
+
+@Override
+protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        }
+
 
 //ボタンが押された時の処理**/
 public void onClick(View v){
@@ -32,4 +41,5 @@ public void startGame(View view) {
         }
         break;
 
+}
 }
